7042824e0184a305680f6fa48ac570e4bf47ceb9
PickyPets <ebrahemmostafa19999@gmail.com>
2026-07-28 09:05:19 +0300

    Move countdown up closer to middle

--- js/index-CNWoJd7e.beautified.js
+++ js/index-CNWoJd7e.beautified.js
@@ -22502,7 +22502,7 @@ function B8({
                 alt: "Letters",
                 className: "w-48 sm:w-56 md:w-64 mb-4 object-contain drop-shadow-md z-20"
             }), p.jsx("div", {
-                className: "mt-4",
+                className: "-mt-12 md:-mt-16",
                 children: p.jsx(uL, {
                     targetDate: e
                 })
